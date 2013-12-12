060ce5fd3eb7f2ae8dc28649114b36dec6ee2d92
Olexiy Zamkoviy <olexiy.z@gmail.com>
2013-12-12 23:42:14 +0200

    Fixed latest Weblocks (0.10.7) compatibility

--- package.lisp
+++ package.lisp
@@ -2,5 +2,6 @@
 
 (defpackage #:weblocks-mustache-template-form-view
   (:use #:cl #:weblocks)
-  (:export #:mustache-template-form-view))
+  (:export #:mustache-template-form-view)
+  (:import-from :weblocks-util #:gen-id #:object-class-name #:safe-apply))
 
